87d77606bab30f06d5c6c2ff58697e41e387c4d5
alexrog <acrogers307@gmail.com>
2020-05-18 22:02:29 -0600

    Updated MA266 page

--- assets/js/MA266.js
+++ assets/js/MA266.js
@@ -1,11 +1,13 @@
 
 var exams = [
     { semester: "2019 Fall", 
+      exam: "final",
       link: "https://www.youtube.com/embed/t9hdIc6yBe4?start=", 
       timestamps: [54,233,388,472,578,785,985,1080,1244,1533,1696,1941,2108,2255,2574,2883,3540,4069,4297,4613],
       description: ["Autonomous eq. stability", "Autonomous eq. stability", "Separation of variables", "Kinematics", "Exact equations", "Exact equations", "Euler's method", "Characteristic equation", "Spring-mass systems", "Undetermined coefficients", "Variation of parameters", "Phase plane identification", "Matrix exponential", "Solve x' = Ax", "Solve x' = Ax + f(t)", "Laplace of a system of eq.", "Piecewise Laplace trans.",	"Laplace Transform", "Laplace IVP", "Laplace IVP"]
     },
     { semester: "2019 Spring",
+      exam: "final",
       link: "https://www.youtube.com/embed/fqET297U3Bc?start=",
       timestamps: [24, 95, 175, 307, 426, 601, 690, 874, 1031, 1228, 1428, 1583, 1699, 1792, 2180, 2396, 2514, 2616, 2784, 2870],
       description: ["Exist. and uniq. theorem",	"Integrating factor",	"Separation of variables",	"Exact equations",	"Characteristic equation",
@@ -14,6 +16,7 @@ var exams = [
     },
     {
       semester: "2018 Fall",
+      exam: "final",
       link: "https://www.youtube.com/embed/BRAxIssBBBM?start=",
       timestamps: [9, 155, 250, 522, 681, 780, 887, 1049, 1283, 1530, 1630, 1687, 1796, 1935, 2179, 2387, 2588, 2830, 2991, 3096],
       description: ["Separation of variables","Salt tank applications","Exact equations","Homogeneous DE","Euler's method",
@@ -22,6 +25,7 @@ var exams = [
     },
     {
       semester: "2018 Spring",
+      exam: "final",
       link: "https://www.youtube.com/embed/bRZmP2tSskg?start=",
       timestamps: [27, 175, 276, 439, 555, 760, 916, 1064, 1221, 1345, 1578, 1640, 1745, 1822, 2277, 2582, 2816, 2953, 3104, 3328],
       description: ["Separation of variables","Euler's method","Exist. and uniq. theorem","Autonomous eq. stability","Homogeneous DE",
@@ -31,6 +35,7 @@ var exams = [
     },
     {
       semester: "2017 Fall",
+      exam: "final",
       link: "https://www.youtube.com/embed/NL7NA_o-904?start=",
       timestamps: [23, 259, 439, 551, 655, 770, 1043, 1199, 1330, 1519, 1648, 1922, 1981, 2115, 2349, 2632, 3007, 3169, 3266, 3445],
       description: ["Autonomous eq. stability","Integrating factor","Separation of variables","Salt tank applications","Euler's method",
@@ -40,6 +45,7 @@ var exams = [
     },
     {
       semester: "2017 Spring",
+      exam: "final",
       link: "https://www.youtube.com/embed/0ZCRCZ8CLio?start=",
       timestamps: [11, 146, 220, 468, 655, 959, 1084, 1189, 1285, 1386, 1638, 1866, 2097, 2293, 2504, 2759, 3027, 3154, 3263, 3496],
       description: ["Integrating factor","Separation of variables","Salt tank applications","v = y / x substitution","Exact equations",
@@ -49,25 +55,72 @@ var exams = [
     }
     ];
 
-function updateVideo() {
-  var semester = document.getElementById("semester").value;
-  var question = document.getElementById("question").value;
-  
-  if(semester != null && question != "Question #") {
-    var foundExam = false;
-    question = parseInt(question);
-  
-    for(var i = 0; i < exams.length; i++) {
-      if(exams[i].semester == semester) {
-        document.getElementById("Video").src = exams[i].link.concat(exams[i].timestamps[question-1]);
-        document.getElementById("video-description").innerText = exams[i].description[question-1];
-        foundExam = true;
+    function buildOnload() {
+      for(var i = 0; i < exams.length; i++) {
+        var sel = document.getElementById("semester");
+          var opt = document.createElement("option");
+            
+          opt.appendChild(document.createTextNode(exams[i].semester));
+          opt.value = exams[i].semester;
+          sel.appendChild(opt);
       }
     }
-
-    if(!foundExam) {
-      document.getElementById("video").src = "";
+    
+    function changeSemester() {
+      document.getElementById("question-default").innerText = "Question #";
+      var semester = document.getElementById("semester").value;
+      var numOpt = document.getElementById("question").length;
+      
+      var numQuestions = 0;
+      for(var i = 0; i < exams.length; i++) {
+        if(semester == exams[i].semester) {
+          numQuestions = exams[i].timestamps.length;
+          break;
+        }
+      }
+    
+      if(numOpt > numQuestions) {
+        var sel = document.getElementById("question");
+        for(i = numOpt; i > numQuestions; i--) {
+          sel.remove(i);
+        }
+      }
+      else {
+        for(var i = numOpt; i <= numQuestions; i++) {
+          var sel = document.getElementById("question");
+          var opt = document.createElement("option");
+            
+          opt.appendChild(document.createTextNode("Question " + i));
+          opt.value = i.toString();
+          sel.appendChild(opt);
+        }
+      }
+    
+      updateVideo();
+    
     }
-  }
-}
+    
+    function updateVideo() {
+      //const numQuestions = 20;
+      var semester = document.getElementById("semester").value;
+      var question = document.getElementById("question").value;
+      var exam = "final"; // placeholder
+    
+      if(semester != null && question != "Question #") {
+        var foundExam = false;
+        question = parseInt(question);
+    
+        for(var i = 0; i < exams.length; i++) {
+          if(exams[i].semester == semester && exams[i].exam == exam) {
+            document.getElementById("video").src = exams[i].link.concat(exams[i].timestamps[question-1]);
+            document.getElementById("video-description").innerText = exams[i].description[question-1];
+            foundExam = true;
+          }
+        }
+    
+        if(!foundExam) {
+          document.getElementById("Video").src = "";
+        }
+      }
+    }
 

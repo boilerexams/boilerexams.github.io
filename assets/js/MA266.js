9361ed813d17c19dcdb95bff5ed8903580003d15
liam <robin502@purdue.edu>
2020-05-23 12:23:26 -0400

    why

--- assets/js/MA266.js
+++ assets/js/MA266.js
@@ -126,24 +126,24 @@ var exams = [
             window.localStorage.setItem('currentQ', examId.concat(' ', semester, ' ', exam, ' Q', question.toString()));
             console.log(window.localStorage.getItem('currentQ'));
             
-            try {
-              console.log("Trying to find a similar topic")
-              for(var j = 0; j < exams.length; j++) {
-                for(var k = 1; k < 20; k++) {
-                  console.log(exams[j].description[k-1], exams[i].description[question-1])
-                  if(exams[i].description[question-1] == exams[j].description[k-1]) {
-                    document.getElementById("video").src = exams[j].link.concat(exams[j].timestamps[k-1])
-                    document.getElementById("video-description").innerText = exams[j].description[k-1];
-                    console.log("Found an exam with an identical topic and new source:")
-                    console.log(exams[j].description[k-1])
-                    console.log(exams[j].link.concat(exams[j].timestamps[k-1]))
-                  }
-                }
-              }
-            }
-            catch {
-              console.log("sad")
-            }
+            // try {
+            //   console.log("Trying to find a similar topic")
+            //   for(var j = 0; j < exams.length; j++) {
+            //     for(var k = 1; k < 20; k++) {
+            //       console.log(exams[j].description[k-1], exams[i].description[question-1])
+            //       if(exams[i].description[question-1] == exams[j].description[k-1]) {
+            //         document.getElementById("video").src = exams[j].link.concat(exams[j].timestamps[k-1])
+            //         document.getElementById("video-description").innerText = exams[j].description[k-1];
+            //         console.log("Found an exam with an identical topic and new source:")
+            //         console.log(exams[j].description[k-1])
+            //         console.log(exams[j].link.concat(exams[j].timestamps[k-1]))
+            //       }
+            //     }
+            //   }
+            // }
+            // catch {
+            //   console.log("sad")
+            // }
           }
     
         if(!foundExam) {
